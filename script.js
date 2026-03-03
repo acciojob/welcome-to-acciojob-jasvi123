75aa906b8a68103d910b6e54377caf61f36ebe6b
jasvi123 <140882695+jasvi123@users.noreply.github.com>
2026-03-03 16:56:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
-document.querySelector("button").addEventListerner("click",function(){
-	alert("Welcome to Accio Jobs!");
-})
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to Accio Jobs!");
+});
